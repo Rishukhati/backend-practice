d7aecfa963b58ec2bd6ca95c89ce3394f8fb724d
Rishabh singh <rishabhsingh.block@gmail.com>
2026-03-13 23:13:27 +0530

    update day 1

--- Day-1/index.js
+++ Day-1/index.js
@@ -3,14 +3,27 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Welcome to my website')
 })
 
-app.get('/twitter', (req,res) =>{
-    res.send('yooo whats up')
+app.get('/about', (req, res) => {
+    res.send('This is the about page')
+})
+
+app.get('/contact', (req, res) => {
+    res.send('Contact me at rishabh@email.com')
+})
+
+app.get('/instagram', (req, res) => {
+    res.send('This is my Instagram page')
+})
+
+app.get('/user/:name', (req, res) => {
+    const username = req.params.name
+    res.send(`Hello ${username}`)
 })
 
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`Server running on port ${port}`)
 })
