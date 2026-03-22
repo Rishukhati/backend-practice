58cf787081d30a6dd045bdc8b2b985401cabf031
Rishabh singh <rishabhsingh.block@gmail.com>
2026-03-22 23:19:17 +0530

    update

--- Day-2/models/todo.models.js
+++ Day-2/models/todo.models.js
@@ -1,7 +1,12 @@
 import mongoose from "mongoose" ;
 
 const todoSchema = new mongoose.Schema({
-    
+    contact: {
+        type: String,
+        required: true,
+        lowercase: true,
+        trim: true,
+    }
 } , {timestamps : true})
 
 export const Todo = mongoose.model("todo" , todoSchema)
